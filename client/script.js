283f6c38415ce9c8169d5cf3b4a657732882c09d
Kant <kant@mail.ru>
2023-01-07 18:49:32 +0300

    Pal

--- client/script.js
+++ client/script.js
@@ -77,7 +77,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from he server
 
-  const response = await fetch("http://localhost:3012", {
+  const response = await fetch("https://openai-1xj7.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
