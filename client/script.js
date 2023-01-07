7ee2944f5c97459bcc2ac472ac9dde583ea40cdd
Kant <kant@mail.ru>
2023-01-07 19:03:43 +0300

    Last last

--- client/script.js
+++ client/script.js
@@ -77,7 +77,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from he server
 
-  const response = await fetch("https://openai-1xj7.onrender.com", {
+  const response = await fetch("http://localhost:3012", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
